68b61747fa625eb4e870157fe7f8119930d7dda1
Vinicius Crespi <vinicrespi@gmail.com>
2020-02-10 19:36:32 -0300

    projeto

---
+++ myOrder/src/test/java/com/example/myOrder/MyOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
